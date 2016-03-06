61cbfe9d9d385d2228590ac93fb0e2ea607dcfd1
captainill <jonthomas0@gmail.com>
2016-03-05 16:03:29 -0800

    add example page

---
+++ src/views/AboutView/AboutView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type Props = {
+
+};
+export class About extends React.Component {
+  props: Props;
+
+  render() {
+    return (
+      <div>About!</div>
+    );
+  }
+}
+
+export default About;
