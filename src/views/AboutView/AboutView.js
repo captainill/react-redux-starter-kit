fa71d316ada039e222f74b73c4385370270b2dd0
captainill <jonthomas0@gmail.com>
2016-03-12 00:22:18 -0800

    upgrade react and refactor some test components. drop in draftjs

--- src/views/AboutView/AboutView.js
+++ src/views/AboutView/AboutView.js
@@ -1,68 +1,30 @@
 import React from 'react';
 import ReactTagSelect from 'react-tag-select';
+import { Editor, EditorState } from 'draft-js';
+import TagPicker from 'components/TagPicker/TagPicker';
 
-require('react-tag-select/dist/style.css');
-
-/* eslint-disable */
-var Countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Anguilla','Antigua &amp; Barbuda','Argentina','Armenia','Aruba','Australia','Austria','Azerbaijan','Bahamas'
-  ,'Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bermuda','Bhutan','Bolivia','Bosnia &amp; Herzegovina','Botswana','Brazil','British Virgin Islands'
-  ,'Brunei','Bulgaria','Burkina Faso','Burundi','Cambodia','Cameroon','Cape Verde','Cayman Islands','Chad','Chile','China','Colombia','Congo','Cook Islands','Costa Rica'
-  ,'Cote D Ivoire','Croatia','Cruise Ship','Cuba','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea'
-  ,'Estonia','Ethiopia','Falkland Islands','Faroe Islands','Fiji','Finland','France','French Polynesia','French West Indies','Gabon','Gambia','Georgia','Germany','Ghana'
-  ,'Gibraltar','Greece','Greenland','Grenada','Guam','Guatemala','Guernsey','Guinea','Guinea Bissau','Guyana','Haiti','Honduras','Hong Kong','Hungary','Iceland','India'
-  ,'Indonesia','Iran','Iraq','Ireland','Isle of Man','Israel','Italy','Jamaica','Japan','Jersey','Jordan','Kazakhstan','Kenya','Kuwait','Kyrgyz Republic','Laos','Latvia'
-  ,'Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Macau','Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Mauritania'
-  ,'Mauritius','Mexico','Moldova','Monaco','Mongolia','Montenegro','Montserrat','Morocco','Mozambique','Namibia','Nepal','Netherlands','Netherlands Antilles','New Caledonia'
-  ,'New Zealand','Nicaragua','Niger','Nigeria','Norway','Oman','Pakistan','Palestine','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal'
-  ,'Puerto Rico','Qatar','Reunion','Romania','Russia','Rwanda','Saint Pierre &amp; Miquelon','Samoa','San Marino','Satellite','Saudi Arabia','Senegal','Serbia','Seychelles'
-  ,'Sierra Leone','Singapore','Slovakia','Slovenia','South Africa','South Korea','Spain','Sri Lanka','St Kitts &amp; Nevis','St Lucia','St Vincent','St. Lucia','Sudan'
-  ,'Suriname','Swaziland','Sweden','Switzerland','Syria','Taiwan','Tajikistan','Tanzania','Thailand','Timor L\'Este','Togo','Tonga','Trinidad &amp; Tobago','Tunisia'
-  ,'Turkey','Turkmenistan','Turks &amp; Caicos','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States of America','Uruguay','Uzbekistan','Venezuela','Vietnam','Virgin Islands (US)'
-  ,'Yemen','Zambia','Zimbabwe'];
-/* eslint-enable */
+require('./AboutView.scss');
 
 type Props = {
 
 };
-export class About extends React.Component {
+export class AboutView extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      tags: [{ id: 1, text: 'Thailand' }, { id: 2, text: 'India' }],
-      suggestions: Countries
-    };
-  }
-
-  props: Props;
-
-  handleDelete = (i) => {
-    const tags = this.state.tags;
-    tags.splice(i, 1);
-    this.setState({ tags });
-  }
-  handleAddition = (tag) => {
-    const tags = this.state.tags;
-    tags.push({
-      id: tags.length + 1,
-      text: tag
-    });
-    this.setState({ tags });
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => this.setState({ editorState });
   }
 
   render() {
     const tags = this.state.tags;
     return (
       <div>
-        <ReactTagSelect tags={tags}
-          suggestions={Countries}
-          handleDelete={this.handleDelete}
-          handleAddition={this.handleAddition}
-          minQueryLength={2}
-        />
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />;
+        <TagPicker />
       </div>
     );
   }
 }
 
-export default About;
+export default AboutView;
---
+++ tests/components/TagPicker.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import TagPicker from 'components/TagPicker/TagPicker'
+
+describe('(Component) TagPicker', () => {
+  it('should exist', () => {
+
+  })
+})
